741f85eed8fd40f0e5fb54a3adf326013e470d88
Mirel P <mirel.palii@gmail.com>
2024-01-06 00:26:12 +0200

    negative validation message

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -41,4 +41,39 @@ describe('TAT Customer Service Center', () => {
       .should('be.visible')
       .and('contain.text', 'Message successfully sent.');
   });
+  it.only('displays an error message when submitting the form with an email with invalid formatting', () => {
+    cy.get('#firstName')
+      .as('firstNameInput')
+      .should('be.visible')
+      .clear()
+      .type('John');
+    cy.get('@firstNameInput').should('have.value', 'John');
+
+    cy.get('#lastName')
+      .as('lastNameInput')
+      .should('be.visible')
+      .clear()
+      .type('Smith');
+    cy.get('@lastNameInput').should('have.value', 'Smith');
+
+    cy.get('#email')
+      .as('emailInput')
+      .should('be.visible')
+      .clear()
+      .type('sjohn..eamail.c');
+    cy.get('@emailInput').should('have.value', 'sjohn..eamail.c');
+
+    cy.get('#open-text-area')
+      .as('feedbackInput')
+      .should('be.visible')
+      .clear()
+      .type('no feedback', { delay: 0 });
+    cy.get('@feedbackInput').should('have.value', 'no feedback');
+
+    cy.get('button[type="submit"]').should('be.visible').click();
+
+    cy.get('.error')
+      .should('be.visible')
+      .and('contain.text', 'Validate the required fields!');
+  });
 });
