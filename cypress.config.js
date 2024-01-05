00cf860596fecd5efa8eb2c72b992302e89729a9
Mirel P <mirel.palii@gmail.com>
2024-01-05 23:56:39 +0200

    get type delay click

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  include: ['./node_modules/cypress', 'cypress/**/*.js'],
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -1,6 +1,44 @@
 describe('TAT Customer Service Center', () => {
-  it('checks the application title', () => {
+  beforeEach(() => {
     cy.visit('../src/index.html');
+  });
+  it('checks the application title', () => {
     cy.title().should('equal', 'TAT Customer Service Center');
+  });
+  it('fills in the required fields and submits the form', () => {
+    cy.get('#firstName')
+      .as('firstNameInput')
+      .should('be.visible')
+      .clear()
+      .type('John');
+    cy.get('@firstNameInput').should('have.value', 'John');
+
+    cy.get('#lastName')
+      .as('lastNameInput')
+      .should('be.visible')
+      .clear()
+      .type('Smith');
+    cy.get('@lastNameInput').should('have.value', 'Smith');
+
+    cy.get('#email')
+      .as('emailInput')
+      .should('be.visible')
+      .clear()
+      .type('sjohn@email.com');
+    cy.get('@emailInput').should('have.value', 'sjohn@email.com');
+
+    const longText = `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to`;
+    cy.get('#open-text-area')
+      .as('feedbackInput')
+      .should('be.visible')
+      .clear()
+      .type(longText, { delay: 0 });
+    cy.get('@feedbackInput').should('have.value', longText);
+
+    cy.get('button[type="submit"]').should('be.visible').click();
+
+    cy.get('.success')
+      .should('be.visible')
+      .and('contain.text', 'Message successfully sent.');
   });
 });
