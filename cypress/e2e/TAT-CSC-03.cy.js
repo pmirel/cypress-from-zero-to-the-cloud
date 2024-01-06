88f4dc15664108f9a42a038479d280d29028ee75
Mirel P <mirel.palii@gmail.com>
2024-01-06 17:50:32 +0200

    select

---
+++ cypress/e2e/TAT-CSC-03.cy.js
@@ -0,0 +1,15 @@
+describe('TAT Customer Service Center', () => {
+  beforeEach(() => {
+    cy.visit('../src/index.html');
+  });
+
+  it('selects a product (YouTube) by its content', () => {
+    cy.get('select').select('YouTube').should('have.value', 'youtube');
+  });
+  it('selects a product (Mentorship) by its value', () => {
+    cy.get('select').select('mentorship').should('have.value', 'mentorship');
+  });
+  it('selects a product (Blog) by its index', () => {
+    cy.get('select').select(1).should('have.value', 'blog');
+  });
+});
