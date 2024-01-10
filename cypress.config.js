1e8d0bf60c2f25840911ab3cc57c2a84359a1459
Mirel P <mirel.palii@gmail.com>
2024-01-10 23:27:22 +0200

    record to cloud from ci

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'd38rg6',
   include: ['./node_modules/cypress', 'cypress/**/*.js'],
   viewportHeight: 880,
   viewportWidth: 1280,
